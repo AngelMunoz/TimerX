98a8d2aba4b94f929cbeb7a2581a82f6adb11c88
Angel D. Munoz <scythes.lol@gmail.com>
2019-05-25 06:14:24 -0600

    feature(auth): added sign in and sign up

--- src/pages/auth/login.ts
+++ src/pages/auth/login.ts
@@ -1,3 +1,31 @@
+import { FireAuth } from 'fire.utils';
+import { autoinject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+
+@autoinject
 export class Login {
 
+  email: string;
+  password: string;
+  unsubscribe: firebase.Unsubscribe;
+
+  constructor(private router: Router) {
+    this.unsubscribe = FireAuth.onAuthStateChanged(user => {
+      if (user) {
+        router.navigateToRoute('home');
+      }
+    });
+  }
+  deactivate() {
+    this.unsubscribe();
+  }
+
+  async login() {
+    try {
+      await FireAuth.signInWithEmailAndPassword(this.email, this.password)
+    } catch (error) {
+      console.warn(error.message);
+      return;
+    }
+  }
 }
--- src/pages/auth/signup.ts
+++ src/pages/auth/signup.ts
@@ -1,3 +1,27 @@
+import { FireAuth } from "fire.utils";
+import { autoinject } from 'aurelia-framework'
+import { Router } from 'aurelia-router'
+@autoinject
 export class SignUp {
 
+  email: string;
+  password: string;
+  repeatedPassword: string;
+  unsubscribe: firebase.Unsubscribe
+
+  constructor(private router: Router) {
+    this.unsubscribe = FireAuth.onAuthStateChanged(user => {
+      if (user) {
+        router.navigateToRoute('home');
+      }
+    })
+  }
+
+  deactivate() {
+    this.unsubscribe();
+  }
+
+  async signup() {
+    await FireAuth.createUserWithEmailAndPassword(this.email, this.password);
+  }
 }
