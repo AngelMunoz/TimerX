8c1b70b0fffd24b081f2489fef03f0d16c234391
Angel D. Munoz <scythes.lol@gmail.com>
2019-05-25 05:07:13 -0600

    build(configuration): add firebase config

--- aurelia_project/environments/dev.ts
+++ aurelia_project/environments/dev.ts
@@ -1,4 +1,13 @@
 export default {
   debug: true,
-  testing: true
+  testing: true,
+  firebase: {
+    apiKey: "AIzaSyANquPcWn48TD8uglLRWkQEV4hDOL4SvtA",
+    authDomain: "timerx-admg.firebaseapp.com",
+    databaseURL: "https://timerx-admg.firebaseio.com",
+    projectId: "timerx-admg",
+    storageBucket: "timerx-admg.appspot.com",
+    messagingSenderId: "511715039019",
+    appId: "1:511715039019:web:2a7caff603dc7d3e"
+  }
 };
--- aurelia_project/environments/prod.ts
+++ aurelia_project/environments/prod.ts
@@ -1,4 +1,13 @@
 export default {
   debug: false,
-  testing: false
+  testing: false,
+  firebase: {
+    apiKey: "AIzaSyANquPcWn48TD8uglLRWkQEV4hDOL4SvtA",
+    authDomain: "timerx-admg.firebaseapp.com",
+    databaseURL: "https://timerx-admg.firebaseio.com",
+    projectId: "timerx-admg",
+    storageBucket: "timerx-admg.appspot.com",
+    messagingSenderId: "511715039019",
+    appId: "1:511715039019:web:3783157d7a1a6704"
+  }
 };
